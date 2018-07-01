e771c235c22bc4ae093d242d93ee5a9a5d5339e7
Lachezar Kostov <lachezarkostov@gmail.com>
2018-07-01 21:39:00 +0100

    InitialCommit

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace GigHub.Controllers
             var upcommingGigs = _context.Gigs
                 .Include(g => g.Artist)
                 .Include(g => g.Genre)
-                .Where(g => g.DateTime > DateTime.Now);
+                .Where(g => g.DateTime > DateTime.Now && g.IsCanceled == false);
             var viewModel = new GigsViewModel
             {
                 UpcomingGigs = upcommingGigs,
